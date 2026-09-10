89adccb5caf324dcb941f7f47ed69a9edf900df7
tuzkimo <tuzkimo@live.com>
2026-09-10 11:34:22 +0800

    feat: 接入 plugin-dialog/plugin-fs 备份数据读写

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,8 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .run(tauri::generate_context!())
